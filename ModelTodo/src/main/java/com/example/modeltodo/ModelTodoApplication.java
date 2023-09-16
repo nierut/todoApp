beb9e550cbd35310f20c693d018f3cdd85aa61e2
Rút Ní Eithir <rut.nieithir@hotmail.com>
2023-09-16 11:29:48 +0200

    updated gitignore

---
+++ ModelTodo/src/main/java/com/example/modeltodo/ModelTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.modeltodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelTodoApplication.class, args);
+    }
+
+}
---
+++ ModelTodo/src/test/java/com/example/modeltodo/ModelTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modeltodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
