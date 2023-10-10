3b82a99a0a460e1b706339d2a7947b6d2913fbe7
Rút Ní Eithir <rut.nieithir@hotmail.com>
2023-10-10 15:20:37 +0200

    creates get all todos method in controller

--- ModelTodo/src/main/java/com/example/modeltodo/controller/TodoController.java
+++ ModelTodo/src/main/java/com/example/modeltodo/controller/TodoController.java
@@ -2,9 +2,13 @@ package com.example.modeltodo.controller;
 
 import com.example.modeltodo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class TodoController {
     final TodoService todoService;
 
@@ -12,6 +16,15 @@ public class TodoController {
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
+
+    @GetMapping("/todos")
+    public ResponseEntity<?> getTodos() {
+        return null;
+    }
+
+
+
+
 
 
 }
